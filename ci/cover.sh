671bc16e7d32daf0e9f0d3b02689383c3b0073d3
vincent <muehler.v@gmail.com>
2017-10-20 18:53:09 +0200

    try to run lcov with absolute dir paths

--- ci/cover.sh
+++ ci/cover.sh
@@ -4,8 +4,8 @@ npm run build-debug
 cd ./test
 npm install --unsafe-perm
 npm run cover
-lcov -c -d ../build/Debug -o coverage/capture.info
-lcov -e coverage/capture.info '*/cc/*' -o coverage/capturedcc.info
-lcov -t opencv4nodejs -a coverage/capturedcc.info -o coverage/opencv4nodejs.info
-genhtml --output-directory coverage coverage/opencv4nodejs.info
+lcov -c -d ../build/Debug -o $(pwd)/coverage/capture.info
+lcov -e coverage/capture.info '*/cc/*' -o $(pwd)/coverage/capturedcc.info
+lcov -t opencv4nodejs -a coverage/capturedcc.info -o $(pwd)/coverage/opencv4nodejs.info
+genhtml --output-directory coverage $(pwd)/coverage/opencv4nodejs.info
 mv coverage/opencv4nodejs.info coverage-report
