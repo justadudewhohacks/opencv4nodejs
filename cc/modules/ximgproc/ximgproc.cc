45fad20d83ef6c49cc94f71024bbb453c17ef8c4
Georgios Rassias <georassias@gmail.com>
2017-11-06 16:13:26 +0200

    Check for opencv version before using SuperpixelLSC/SLIC

--- cc/modules/ximgproc/ximgproc.cc
+++ cc/modules/ximgproc/ximgproc.cc
@@ -7,8 +7,10 @@
 
 NAN_MODULE_INIT(XImgproc::Init) {
 	SuperpixelSEEDS::Init(target);
+#if OPENCV_MINOR_VERSION > 0
 	SuperpixelSLIC::Init(target);
 	SuperpixelLSC::Init(target);
+#endif
 }
 
 #endif // HAVE_XIMGPROC
