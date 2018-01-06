73bf7fb98c5544096562d97cc067a4c33716c4d8
michaljarnot <michal.jarnot@strv.com>
2018-01-06 18:24:41 +0100

    Add template matching example

---
+++ examples/templateMatching.js
@@ -0,0 +1,31 @@
+const cv = require('../');
+
+
+const findWaldo = async () => {
+  // Load images
+  const originalMat = await cv.imreadAsync(`${__dirname}/templateMatching/original.jpg`);
+  const waldoMat = await cv.imreadAsync(`${__dirname}/templateMatching/waldo.jpg`);
+
+  // Match template (the brightest locations indicate the highest match)
+  const matched = originalMat.matchTemplate(waldoMat, 5);
+
+  // Use minMaxLoc to locate the highest value (or lower, depending of the type of matching method)
+  const minMax = matched.minMaxLoc();
+  const { maxLoc: { x, y } } = minMax;
+
+  // Draw bounding rectangle
+  originalMat.drawRectangle(
+    new cv.Point(x, y),
+    new cv.Point(x + waldoMat.cols, y + waldoMat.rows),
+    new cv.Vec(0, 255, 0),
+    cv.LINE_8,
+    2
+  );
+
+  // Open result in new window
+  cv.imshow('We\'ve found Waldo!', originalMat);
+  cv.waitKey(7000);
+};
+
+// noinspection JSIgnoredPromiseFromCall
+findWaldo();
