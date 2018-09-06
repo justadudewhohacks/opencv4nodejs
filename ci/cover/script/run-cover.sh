4626bea3f9b3821d0c069eaed3e4ff649b5f7894
vincent <muehler.v@gmail.com>
2018-09-06 09:55:47 +0200

    travis - fixed exit and fail when tests fail

--- ci/cover/script/run-cover.sh
+++ ci/cover/script/run-cover.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 export OPENCV4NODEJS_DISABLE_AUTOBUILD=1
-npm install --unsafe-perm
-npm run build-debug
-cd ./test
-npm install --unsafe-perm
-npm run cover
-lcov -c -d ../build/Debug -o $(pwd)/coverage/capture.info
-lcov -e coverage/capture.info '*/cc/*' -o $(pwd)/coverage/capturedcc.info
-lcov -t opencv4nodejs -a coverage/capturedcc.info -o $(pwd)/coverage/opencv4nodejs.info
-genhtml --output-directory coverage $(pwd)/coverage/opencv4nodejs.info
+npm install --unsafe-perm &&\
+npm run build-debug &&\
+cd ./test &&\
+npm install --unsafe-perm &&\
+npm run cover &&\
+lcov -c -d ../build/Debug -o $(pwd)/coverage/capture.info &&\
+lcov -e coverage/capture.info '*/cc/*' -o $(pwd)/coverage/capturedcc.info &&\
+lcov -t opencv4nodejs -a coverage/capturedcc.info -o $(pwd)/coverage/opencv4nodejs.info &&\
+genhtml --output-directory coverage $(pwd)/coverage/opencv4nodejs.info &&\
 mv coverage/opencv4nodejs.info coverage-report
-exit $?
--- ci/test/script/run-test.sh
+++ ci/test/script/run-test.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 export OPENCV4NODEJS_DISABLE_AUTOBUILD=1
-echo installing
-npm install --unsafe-perm
-echo running tests
-cd ./test
-npm install --unsafe-perm
-npm run test-docker
+echo installing &&\
+npm install --unsafe-perm &&\
+echo running tests &&\
+cd ./test &&\
+npm install --unsafe-perm &&\
+npm run test-docker &&\
 npm run test-externalMemTracking
-exit $?
