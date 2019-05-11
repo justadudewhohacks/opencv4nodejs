dac75d597e6e262c1c917d9d035657be9ad67dbc
vincent <muehler.v@gmail.com>
2019-05-11 20:39:35 +0200

    fixed guard for cv::text methods, which are not available in 3.0.0

--- cc/modules/text/textBindings.h
+++ cc/modules/text/textBindings.h
@@ -9,48 +9,51 @@ namespace TextBindings {
   public:
     cv::Ptr <cv::text::OCRHMMDecoder::ClassifierCallback> classifier;
     std::string file;
-  
+
     FF_VAL getReturnValue() {
       FF_OBJ jsClassifier = FF::newInstance(Nan::New(OCRHMMClassifier::constructor));
       FF_UNWRAP(jsClassifier, OCRHMMClassifier)->classifier = classifier;
       return jsClassifier;
     }
-  
+
     bool unwrapRequiredArgs(Nan::NAN_METHOD_ARGS_TYPE info) {
       return StringConverter::arg(0, &file, info);
     }
   };
-  
+
   struct LoadOCRHMMClassifierNMWorker : public LoadOCRHMMClassifierWorker {
     std::string executeCatchCvExceptionWorker() {
       classifier = cv::text::loadOCRHMMClassifierNM(file);
       return "";
     }
   };
-  
+
+
+#if CV_MINOR_VERSION > 0
+
   struct LoadOCRHMMClassifierCNNWorker : public LoadOCRHMMClassifierWorker {
     std::string executeCatchCvExceptionWorker() {
       classifier = cv::text::loadOCRHMMClassifierCNN(file);
       return "";
     }
   };
-  
+
   struct CreateOCRHMMTransitionsTableWorker : public CatchCvExceptionWorker {
   public:
     std::string vocabulary;
     std::vector<std::string> lexicon;
-  
+
     cv::Mat transition_probabilities_table;
-  
+
     std::string executeCatchCvExceptionWorker() {
       cv::text::createOCRHMMTransitionsTable(vocabulary, lexicon, transition_probabilities_table);
       return "";
     }
-  
+
     FF_VAL getReturnValue() {
       return Mat::Converter::wrap(transition_probabilities_table);
     }
-  
+
     bool unwrapRequiredArgs(Nan::NAN_METHOD_ARGS_TYPE info) {
       return (
         StringConverter::arg(0, &vocabulary, info) ||
@@ -58,7 +61,8 @@ namespace TextBindings {
       );
     }
   };
-  
+
+#endif
 
 }
 
