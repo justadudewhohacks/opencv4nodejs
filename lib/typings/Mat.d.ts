d2eac5f83f3e5a59316330106b395f75bee07cc9
Pierre Colle <piercus@gmail.com>
2019-05-15 11:11:38 +0200

    undistort: update documentation

--- lib/typings/Mat.d.ts
+++ lib/typings/Mat.d.ts
@@ -310,6 +310,8 @@ export class Mat {
   transpose(): Mat;
   triangulatePoints(projPoints1: Point2[], projPoints2: Point2[]): Mat;
   triangulatePointsAsync(projPoints1: Point2[], projPoints2: Point2[]): Promise<Mat>;
+  undistort(cameraMatrix: Mat, distCoeffs: Mat): Mat;
+  undistortAsync(cameraMatrix: Mat, distCoeffs: Mat): Promise<Mat>;
   validateDisparity(cost: Mat, minDisparity: number, numberOfDisparities: number, disp12MaxDisp?: number): void;
   validateDisparityAsync(cost: Mat, minDisparity: number, numberOfDisparities: number, disp12MaxDisp?: number): Promise<void>;
   warpAffine(transforMationMatrix: Mat, size?: Size, flags?: number, borderMode?: number, borderValue?: Vec3): Mat;
