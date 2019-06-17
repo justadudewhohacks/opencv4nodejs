e739502cac032690cdbf9c7c0e5771e8aaf3ffce
vincent <muehler.v@gmail.com>
2019-06-17 10:58:43 +0200

    pass TEST_MODULE_LIST env to docker builds

--- ci/test/test.sh
+++ ci/test/test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 image=opencv4nodejs-ci:$1-node$2
 docker build -t $image -f ./Dockerfile --build-arg TAG=$1 --build-arg NODE_MAJOR_VERSION=$2 ../../
-docker run $image
+docker run $image -e TEST_MODULE_LIST=$TEST_MODULE_LIST
