816fd6a4c243ce5c9a36a3cfeeeb0caf4352d76e
Glen Keane <glenkeane.94@gmail.com>
2019-06-24 22:41:24 +0100

    missing file

---
+++ lib/typings/Vec6.d.ts
@@ -0,0 +1,11 @@
+import { Vec } from './Vec.d';
+
+export class Vec6 extends Vec {
+  readonly u: number;
+  readonly v: number;
+  readonly w: number;
+  readonly x: number;
+  readonly y: number;
+  readonly z: number;
+  constructor(u: number, v: number, w: number, x: number, y: number, z: number);
+}
