e4cff82d9a955f45db9f62b7654ee1eeb20702d7
Achim Königs <garfonso@mobo.info>
2020-04-15 18:05:50 +0200

    add version idef guards

--- cc/io/io.cc
+++ cc/io/io.cc
@@ -16,7 +16,9 @@ NAN_MODULE_INIT(Io::Init) {
   Nan::SetMethod(target, "imshowWait", ImshowWait);
   Nan::SetMethod(target, "imwrite", Imwrite);
   Nan::SetMethod(target, "waitKey", WaitKey);
+#if CV_VERSION_GREATER_EQUAL(3, 2, 0)
   Nan::SetMethod(target, "waitKeyEx", WaitKeyEx);
+#endif
   Nan::SetMethod(target, "imencode", Imencode);
   Nan::SetMethod(target, "imdecode", Imdecode);
   Nan::SetMethod(target, "moveWindow", MoveWindow);
@@ -106,6 +108,7 @@ NAN_METHOD(Io::WaitKey) {
   info.GetReturnValue().Set(Nan::New(key));
 }
 
+#if CV_VERSION_GREATER_EQUAL(3, 2, 0)
 NAN_METHOD(Io::WaitKeyEx) {
   int key;
   if (info[0]->IsNumber()) {
@@ -115,6 +118,7 @@ NAN_METHOD(Io::WaitKeyEx) {
   }
   info.GetReturnValue().Set(Nan::New(key));
 }
+#endif
 
 NAN_METHOD(Io::MoveWindow) {
 	FF::TryCatch tryCatch("Io::MoveWindow");
