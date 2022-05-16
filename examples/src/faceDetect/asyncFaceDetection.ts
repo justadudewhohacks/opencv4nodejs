723747fc7ca3cae7f9e91da5acc5613d15c74c6a
urielch <uriel.chemouni@gmail.com>
2022-05-16 13:24:51 +0300

    refactor asyncFaceDetection.ts

--- examples/src/faceDetect/asyncFaceDetection.ts
+++ examples/src/faceDetect/asyncFaceDetection.ts
@@ -1,32 +1,28 @@
 import { cv, getResourcePath, drawBlueRect, wait4key } from '../utils';
 
-const classifier = new cv.CascadeClassifier(cv.HAAR_FRONTALFACE_ALT2);
-cv.imreadAsync(getResourcePath('got.jpg'))
-  .then(img =>
-    img.bgrToGrayAsync()
-      .then(grayImg => classifier.detectMultiScaleAsync(grayImg))
-      .then(
-        (res) => {
-          const { objects, numDetections } = res;
-          if (!objects.length) {
-            return Promise.reject('No faces detected!');
-          }
+export async function asyncFaceDetection() {
+  const classifier = new cv.CascadeClassifier(cv.HAAR_FRONTALFACE_ALT2);
+  try {
+    const img = await cv.imreadAsync(getResourcePath('got.jpg'))
+    const grayImg = await img.bgrToGrayAsync()
+    const res = await classifier.detectMultiScaleAsync(grayImg)
+    const { objects, numDetections } = res;
+    if (!objects.length) {
+      return Promise.reject('No faces detected!');
+    }
+    // draw detection
+    const facesImg = img.copy();
+    const numDetectionsTh = 10;
+    console.log(`detectMultiScale Found ${objects.length} objects`);
+    objects.forEach((rect, i) => {
+      const thickness = numDetections[i] < numDetectionsTh ? 1 : 2;
+      drawBlueRect(facesImg, rect, { thickness });
+    });
+    cv.imshow('face detection', facesImg);
+    await wait4key();
+  } catch (err) { 
+    console.error(err);
+  }
+}
 
-          // draw detection
-          const facesImg = img.copy();
-          const numDetectionsTh = 10;
-          objects.forEach((rect, i) => {
-            const thickness = numDetections[i] < numDetectionsTh ? 1 : 2;
-            drawBlueRect(facesImg, rect, { thickness });
-          });
-
-          return facesImg;
-        }
-      )
-      .then((facesImg) => {
-        cv.imshow('face detection', facesImg);
-        return wait4key();
-      })
-    )
-    .catch(err => console.error(err));
-
+asyncFaceDetection();
