6af966e8eb501a9a2f4c58236964253876842405
urielch <uriel.chemouni@gmail.com>
2022-05-31 13:56:45 +0300

    add comment in macro header.

--- cc/core/matUtils.h
+++ cc/core/matUtils.h
@@ -106,6 +106,9 @@
 	}
 
 namespace FF {
+	/**
+	 * 2,3-Dimmentions Macro seters for a single Value
+	 */
 	template<typename type>
 	static inline void matPutVal(cv::Mat mat, v8::Local<v8::Value> value, int r, int c) {
 		mat.at<type>(r, c) = (type)value->ToNumber(Nan::GetCurrentContext()).ToLocalChecked()->Value();
@@ -116,6 +119,9 @@ namespace FF {
 		mat.at<type>(r, c, z) = (type)value->ToNumber(Nan::GetCurrentContext()).ToLocalChecked()->Value();
 	}
 
+	/**
+	 * 2,3-Dimmentions Macro seters for a Vec<2> Value
+	 */
 	template<typename type>
 	static inline void matPutVec2(cv::Mat mat, v8::Local<v8::Value> vector, int r, int c) {
 		v8::Local<v8::Array> vec = v8::Local<v8::Array>::Cast(vector);
@@ -133,7 +139,9 @@ namespace FF {
 			(type)FF::DoubleConverter::unwrapUnchecked(Nan::Get(vec, 1).ToLocalChecked())
 		);
 	}
-
+	/**
+	 * 2,3-Dimmentions Macro seters for a Vec<3> Value
+	 */
 	template<typename type>
 	static inline void matPutVec3(cv::Mat mat, v8::Local<v8::Value> vector, int r, int c) {
 		v8::Local<v8::Array> vec = v8::Local<v8::Array>::Cast(vector);
@@ -153,7 +161,9 @@ namespace FF {
 			(type)FF::DoubleConverter::unwrapUnchecked(Nan::Get(vec, 2).ToLocalChecked())
 		);
 	}
-
+	/**
+	 * 2,3-Dimmentions Macro seters for a Vec<4> Value
+	 */
 	template<typename type>
 	static inline void matPutVec4(cv::Mat mat, v8::Local<v8::Value> vector, int r, int c) {
 		v8::Local<v8::Array> vec = v8::Local<v8::Array>::Cast(vector);
@@ -176,6 +186,9 @@ namespace FF {
 		);
 	}
 
+	/**
+	 * 2,3,n-Dimmentions Macro getters for single Value
+	 */
 	template<typename type>
 	static inline v8::Local<v8::Value> matGetVal(cv::Mat mat, int r, int c) {
 		return Nan::New(mat.at<type>(r, c));
@@ -186,11 +199,14 @@ namespace FF {
 		return Nan::New(mat.at<type>(r, c, z));
 	}
 
-  template<typename type>
+	template<typename type>
 	static inline v8::Local<v8::Value> matGetVal(cv::Mat mat, const int* idx) {
 		return Nan::New(mat.at<type>(idx));
 	}
 
+	/**
+	 * 2,3,n-Dimmentions Macro getters for Vec<2> Value
+	 */
 	template<typename type>
 	static inline v8::Local<v8::Value> matGetVec2(cv::Mat mat, int r, int c) {
 		v8::Local<v8::Array> vec = Nan::New<v8::Array>(2);
@@ -207,7 +223,7 @@ namespace FF {
 		return vec;
 	}
 
-  template<typename type>
+	template<typename type>
 	static inline v8::Local<v8::Value> matGetVec2(cv::Mat mat, const int* idx) {
 		v8::Local<v8::Array> vec = Nan::New<v8::Array>(2);
 		Nan::Set(vec, 0, Nan::New(mat.at< cv::Vec<type, 2> >(idx)[0]));
@@ -215,6 +231,9 @@ namespace FF {
 		return vec;
 	}
 
+	/**
+	 * 2,3,n-Dimmentions Macro getters for Vec<3> Value
+	 */
 	template<typename type>
 	static inline v8::Local<v8::Value> matGetVec3(cv::Mat mat, int r, int c) {
 		v8::Local<v8::Array> vec = Nan::New<v8::Array>(3);
@@ -233,7 +252,7 @@ namespace FF {
 		return vec;
 	}
 
-  template<typename type>
+    template<typename type>
 	static inline v8::Local<v8::Value> matGetVec3(cv::Mat mat, const int* idx) {
 		v8::Local<v8::Array> vec = Nan::New<v8::Array>(3);
 		Nan::Set(vec, 0, Nan::New(mat.at< cv::Vec<type, 3> >(idx)[0]));
@@ -242,6 +261,9 @@ namespace FF {
 		return vec;
 	}
 
+	/**
+	 * 2,3,n-Dimmentions Macro getters for Vec<4> Value
+	 */
 	template<typename type>
 	static inline v8::Local<v8::Value> matGetVec4(cv::Mat mat, int r, int c) {
 		v8::Local<v8::Array> vec = Nan::New<v8::Array>(4);
@@ -262,15 +284,15 @@ namespace FF {
 		return vec;
 	}
 
-  template<typename type>
-  static inline v8::Local<v8::Value> matGetVec4(cv::Mat mat, const int* idx) {
-    v8::Local<v8::Array> vec = Nan::New<v8::Array>(4);
-    Nan::Set(vec, 0, Nan::New(mat.at< cv::Vec<type, 4> >(idx)[0]));
-    Nan::Set(vec, 1, Nan::New(mat.at< cv::Vec<type, 4> >(idx)[1]));
-    Nan::Set(vec, 2, Nan::New(mat.at< cv::Vec<type, 4> >(idx)[2]));
-    Nan::Set(vec, 3, Nan::New(mat.at< cv::Vec<type, 4> >(idx)[3]));
-    return vec;
-  }
+	template<typename type>
+	static inline v8::Local<v8::Value> matGetVec4(cv::Mat mat, const int* idx) {
+		v8::Local<v8::Array> vec = Nan::New<v8::Array>(4);
+		Nan::Set(vec, 0, Nan::New(mat.at< cv::Vec<type, 4> >(idx)[0]));
+		Nan::Set(vec, 1, Nan::New(mat.at< cv::Vec<type, 4> >(idx)[1]));
+		Nan::Set(vec, 2, Nan::New(mat.at< cv::Vec<type, 4> >(idx)[2]));
+		Nan::Set(vec, 3, Nan::New(mat.at< cv::Vec<type, 4> >(idx)[3]));
+		return vec;
+	}
 }
 
 #endif
