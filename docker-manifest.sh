aefd72cdca37939b1e4089f73286792f73fb9bbc
urielch <uriel.chemouni@gmail.com>
2022-11-06 22:15:32 +0200

    improve docker build

--- docker-manifest.sh
+++ docker-manifest.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 VERSION="6.2.5"
 IMG=urielch/opencv-nodejs;
-BASE=debian
+VARIANT="-debian"
+VARIANT="-alpine"
 RED="\e[31m"
 GREEN="\e[32m"
 NC="\e[0m"
@@ -25,33 +26,33 @@ then
  printf "version_number \"${GREEN}${VERSION}${NC}\" must be a like 1.0.0\n"
  exit 1
 fi
-
 set -e
-
-printf \n"Building ${RED}${IMG}${NC} version \"${GREEN}${BASE}-${VERSION}${NC}\"\n\n"
-
 ARCH=$(arch)
 if [ $ARCH == 'aarch64' ]; then ARCH=arm64; fi
 if [ $ARCH == 'x86_64'  ]; then ARCH=amd64; fi
 
+printf "\nBuilding ${RED}${IMG}${NC} version \"${GREEN}${VERSION}${VARIANT}${NC}\"\n\n"
 # prebuild image:
-time docker build --build-arg VERSION=${VERSION} --pull --rm -f Dockerfile-${BASE}  -t ${IMG}:${VERSION}-${BASE}-${ARCH} .
+time docker build --build-arg VERSION=${VERSION} --pull --rm -f Dockerfile${VARIANT}  -t ${IMG}:${VERSION}${VARIANT}-${ARCH} .
 
-printf "Pushing Image ${RED}:${VERSION}-${BASE}-${ARCH}${NC}\n"
-docker push ${IMG}:${VERSION}-${BASE}-${ARCH}
+printf "Pushing Image ${RED}:${VERSION}${VARIANT}-${ARCH}${NC}\n"
+docker push ${IMG}:${VERSION}${VARIANT}-${ARCH}
 
-printf "Image ${RED}${IMG}${NC}:${GREEN}${VERSION}-${BASE}-${ARCH}${NC} Ready\n"
-printf "Building manifest for version \"${GREEN}${VERSION}-${BASE}${NC}\"\n\n"
+printf "Image ${RED}${IMG}${NC}:${GREEN}${VERSION}${VARIANT}-${ARCH}${NC} Ready\n"
+printf "Building manifest for version \"${GREEN}${VERSION}${VARIANT}${NC}\"\n\n"
 
-for FINAL in ${IMG}:${VERSION}-${BASE} ${IMG}:latest
+TO_PUSH=(${IMG}:${VERSION}${VARIANT})
+[ ${VARIANT} == '-debian' ] && TO_PUSH+=(${IMG}:latest)
+[ ${VARIANT} == '-debian' ] && TO_PUSH+=(${IMG}:${VERSION})
+for FINAL in ${TO_PUSH[@]}
 do
   docker manifest rm       ${FINAL} 2> /dev/null || true
-  docker manifest create   ${FINAL} ${IMG}:${VERSION}-${BASE}-arm64 ${IMG}:${VERSION}-${BASE}-amd64;
-  docker manifest annotate ${FINAL} ${IMG}:${VERSION}-${BASE}-arm64 --arch arm64;
-  docker manifest annotate ${FINAL} ${IMG}:${VERSION}-${BASE}-amd64 --arch amd64;
+  docker manifest create   ${FINAL} ${IMG}:${VERSION}${VARIANT}-arm64 ${IMG}:${VERSION}${VARIANT}-amd64;
+  docker manifest annotate ${FINAL} ${IMG}:${VERSION}${VARIANT}-arm64 --arch arm64;
+  docker manifest annotate ${FINAL} ${IMG}:${VERSION}${VARIANT}-amd64 --arch amd64;
   docker manifest push     ${FINAL};
   docker manifest inspect  ${FINAL};
 done
 
-printf "${RED}${IMG}${NC} VERSION ${GREEN}${VERSION}-${BASE}${NC} is now published. You can safely delete single arch tags from:\n"
+printf "${RED}${IMG}${NC} VERSION ${GREEN}${VERSION}${VARIANT}${NC} is now published. You can safely delete single arch tags from:\n"
 printf "${GREEN}https://hub.docker.com/repository/docker/${IMG}/tags${NC}\n"
