8b00a9fe35a61c6e0b153cd83ac3ce66666e5269
UrielCh <uriel.chemouni@gmail.com>
2022-11-10 20:42:00 +0200

    try new docker-manifest

--- docker-manifest.sh
+++ docker-manifest.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
+# start MacOS docker:
+# open -a /Applications/Docker.app
+# security -v unlock-keychain ~/Library/Keychains/login.keychain-db
+# setip doc: https://github.com/docker/for-mac/issues/6504
 VERSION="6.2.5"
 IMG=urielch/opencv-nodejs;
 RED="\e[31m"
 GREEN="\e[32m"
 NC="\e[0m"
 VARIANTS=(-debian -alpine)
+ARCHS=(arm64 amd64)
 LATEST_VARIANT=${VARIANTS[0]}
+NAMESPACE=${IMG%/*}
+REPO=${IMG#*/}
+TOKEN=''
+DOCKER_LOGIN="https://hub.docker.com/v2/users/login"
+DOCKER_TAG="https://hub.docker.com/v2/namespaces/${NAMESPACE}/repositories/${REPO}/tags"
 
+if [ ! -z "${DOCKER_CLEAN_TOKEN}" ]
+then
+  login_data() {
+  cat <<EOF
+{ "username": "${DOCKER_USERNAME}", "password": "${DOCKER_CLEAN_TOKEN}" }
+EOF
+  }
+  TOKEN=`curl -s -H "Content-Type: application/json" -X POST -d "$(login_data)" "${DOCKER_LOGIN}" | jq -r .token`
+fi
 
 # $(grep '"version"' package.json | cut -d : -f2 | cut '-d"' -f2)
 
@@ -15,6 +34,11 @@ then
  printf "Usage ${GREEN}${0} version_number${NC}\n"
  # echo "vesion number not profig using ${VERSION} from package.json"
  printf "Check previous version at https://hub.docker.com/repository/docker/${IMG}\n"
+ if [ ! -z "${TOKEN}" ]
+ then
+  echo last tags:
+  curl -s "${DOCKER_TAG}?page_size=5" -H "Authorization: JWT ${TOKEN}" | jq -r .results[].name
+ fi
  exit 1;
 else
  VERSION="$1"
@@ -53,9 +77,13 @@ do
   for FINAL in ${TO_PUSH[@]}
   do
     docker manifest rm       ${FINAL} 2> /dev/null || true
-    docker manifest create   ${FINAL} ${IMG}:${VERSION}${VARIANT}-arm64 ${IMG}:${VERSION}${VARIANT}-amd64;
-    docker manifest annotate ${FINAL} ${IMG}:${VERSION}${VARIANT}-arm64 --arch arm64;
-    docker manifest annotate ${FINAL} ${IMG}:${VERSION}${VARIANT}-amd64 --arch amd64;
+    # docker manifest create   ${FINAL} ${IMG}:${VERSION}${VARIANT}-arm64 ${IMG}:${VERSION}${VARIANT}-amd64;
+
+    CREATE_CMD=(docker manifest create ${FINAL})
+    for ARCH in "${ARCHS[@]}"; do CREATE_CMD+=(${IMG}:${VERSION}${VARIANT}-${ARCH}); done
+    ${CREATE_CMD[@]}
+    
+    for ARCH in "${ARCHS[@]}"; do docker manifest annotate ${FINAL} ${IMG}:${VERSION}${VARIANT}-${ARCH} --arch ${ARCH}; done
     docker manifest push     ${FINAL};
     docker manifest inspect  ${FINAL};
   done
@@ -63,3 +91,19 @@ do
 done
 
 printf "You can safely delete single arch tags from:\n${GREEN}https://hub.docker.com/repository/docker/${IMG}/tags${NC}\n"
+
+if [ ! -z "${TOKEN}" ]
+then
+  printf "${RED}Deleting${NC} temporary tags."
+  echo Deleting TMP tags
+  for VARIANT in "${VARIANTS[@]}"
+  do
+    for ARCH in "${ARCHS[@]}"
+    do 
+     echo DELETE "${DOCKER_TAG}/${VERSION}${VARIANT}-${ARCH}"
+     curl -X DELETE -H "Authorization: JWT ${TOKEN}" "${DOCKER_TAG}/${VERSION}${VARIANT}-${ARCH}"
+   done
+  done 
+fi
+
+printf "${GREEN}cleanUP done.${NC}"
