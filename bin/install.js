7d4d4a58f37652aed1b3c42be663ef0c7b4f61a0
UrielCh <uriel.chemouni@gmail.com>
2022-12-30 08:30:27 +0200

    try again

--- bin/install.js
+++ bin/install.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
 
+console.log('LOADING ../install/compileLib.js from install.js')
+console.log('LOADING ../install/compileLib.js from install.js')
 const compileLib = require("../install/compileLib.js");
 compileLib.compileLib(process.argv);
