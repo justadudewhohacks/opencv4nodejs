b3470e7c8c9b1b73f2f283b05f34f90275e79285
ht-jo <ht.jo@navercorp.com>
2023-01-06 13:30:23 +0900

    Clean up code for new forked repository

--- cc/xfeatures2d/xfeatures2d.cc
+++ cc/xfeatures2d/xfeatures2d.cc
@@ -3,11 +3,11 @@
 #ifdef HAVE_OPENCV_XFEATURES2D
 
 #include "xfeatures2d.h"
-// #include "SIFTDetector.h" https://github.com/justadudewhohacks/opencv4nodejs/issues/805#issuecomment-806314329
+#include "SIFTDetector.h"
 #include "SURFDetector.h"
 
 NAN_MODULE_INIT(XFeatures2d::Init) {
-	// SIFTDetector::Init(target); https://github.com/justadudewhohacks/opencv4nodejs/issues/805#issuecomment-806314329
+	SIFTDetector::Init(target);
 	SURFDetector::Init(target);
 };
 
